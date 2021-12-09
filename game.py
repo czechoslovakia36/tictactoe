40ec711ca2cbe6844f2a377a534d799a0a3f3a89
Vivek Kumar <czechoslovakia36@gmail.com>
2021-12-09 15:52:28 +0500

    code done .. bug needed to be fixed

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+from player import HumanPlayer, RandomComputerPlayer
+
 class TicTacToe:
     def __init__(self):
         self.board=['' for _ in range(9)] # we will use a single list to rep 3x3 Board
@@ -14,7 +16,7 @@ class TicTacToe:
 
     def available_moves(self):
         return [i for i,spot in enumerate(self.board) if spot==' ']
-        moves=[]
+        # moves=[]
         # for (i,spot) in enumerate(self.board):
         #     if spot== '':
         #         moves.append(i)
@@ -40,6 +42,33 @@ class TicTacToe:
     def winner(self,square,letter):
         # winner if 3 in a row anywhere..we have to check all of these..
         # first let's check the row
+        row_ind=square//3
+        row= self.board[row_ind*3: (row_ind+1)*3]
+        if all([spot==letter for spot in row]):
+            return True
+        #check Column
+        col_ind= square % 3
+        column= [self.board[col_ind+i*3] for i in range(3) ]
+        if all([spot==letter for spot in column]):
+            return True 
+        #check diagonals
+        # but only if the square is an even number (0,2,4,6,8)
+        #  these are the only moves possible to win a diagonal
+
+        if square%2 == 0:
+            diagonal1=[self.board[i] for i in [0,4,8]] # left to right diagonal
+            if all([spot==letter for spot in diagonal1]):
+                return True
+
+            diagonal2=[self.board[i] for i in [2,4,6]]
+            if all([spot==letter for spot in diagonal2]):
+                return True 
+        # if all of these fail
+        return False 
+
+
+
+
         
 
 def play(game,x_player,o_player,print_game=True):
@@ -85,4 +114,9 @@ def play(game,x_player,o_player,print_game=True):
 
 
 
+if __name__ =='__main__':
+    x_player=HumanPlayer('X')
+    o_player=RandomComputerPlayer('O')
+    t=TicTacToe()
+    play(t,x_player,o_player,print_game=True)
 
